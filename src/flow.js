b6647a38d7148b64741115c44ad0d51558e6e61e
Zigopis Nikolaos <nzigopis@gmail.com>
2026-03-04 12:50:22 +0200

    Create src/flow.js with screen navigation logic

---
+++ src/flow.js
@@ -0,0 +1,22 @@
+// flow.js
+
+// Screen Navigation Logic
+
+const navigateToScreen = (screen) => {
+    switch (screen) {
+        case 'home':
+            // Logic to navigate to home
+            break;
+        case 'profile':
+            // Logic to navigate to profile
+            break;
+        case 'settings':
+            // Logic to navigate to settings
+            break;
+        default:
+            // Logic for unknown screen
+            break;
+    }
+};
+
+export default navigateToScreen;
